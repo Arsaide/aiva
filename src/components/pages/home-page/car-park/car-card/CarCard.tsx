3dd9d812ff1942779fd1bec66a6e7a8c87607d2f
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-03-23 23:31:40 +0200

    - added modal window structure

--- src/components/pages/home-page/car-park/car-card/CarCard.tsx
+++ src/components/pages/home-page/car-park/car-card/CarCard.tsx
@@ -9,21 +9,49 @@ interface ICarCard {
 
 const CarCard: FC<ICarCard> = ({ card }) => {
     return (
-        <div>
-            <Image src={card.img} alt={card.alt} width={300} height={200} />
-            <h5>{card.title}</h5>
-            <p>{card.desc}</p>
-            <ul>
-                <li>{card.carrying}</li>
-                <li>{card.height}</li>
-                <li>{card.width}</li>
-                <li>{card.length}</li>
-                {card.volume && <li>{card.volume}</li>}
-                <li>{card.body}</li>
-                <li>{card.loading}</li>
-                {card.truckLoading && <li>{card.truckLoading}</li>}
-                {card.capacity && <li>{card.capacity}</li>}
-            </ul>
+        <div className={styles.content}>
+            <div className={styles.image}>
+                <Image src={card.img} alt={card.alt} width={254} height={171} />
+                <h5 className={styles.title}>{card.title}</h5>
+            </div>
+            <div className={styles.props}>
+                <p className={styles.desc}>{card.desc}</p>
+                <ul className={styles.propsList}>
+                    <li className={styles.prop}>
+                        <strong>Грузоподъемность</strong> - {card.carrying}
+                    </li>
+                    <li className={styles.prop}>
+                        <strong>Высота</strong> - {card.height}
+                    </li>
+                    <li className={styles.prop}>
+                        <strong>Ширина</strong> - {card.width}
+                    </li>
+                    <li className={styles.prop}>
+                        <strong>Длина</strong> - {card.length}
+                    </li>
+                    {card.volume && (
+                        <li className={styles.prop}>
+                            <strong>Объем</strong> - {card.volume}
+                        </li>
+                    )}
+                    <li className={styles.prop}>
+                        <strong>Кузов</strong> - {card.body}
+                    </li>
+                    <li className={styles.prop}>
+                        <strong>Погрузка</strong> - {card.loading}
+                    </li>
+                    {card.truckLoading && (
+                        <li className={styles.prop}>
+                            <strong>Погрузка фуры</strong> - {card.truckLoading}
+                        </li>
+                    )}
+                    {card.capacity && (
+                        <li className={styles.prop}>
+                            <strong>Вместимость</strong> - {card.capacity}
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
