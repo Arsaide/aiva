99e86e6683dddc7be7cbc95d3466b8fb2390c0b6
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-03-24 16:22:38 +0200

    - worked with banner components

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Main from '@/components/pages/home-page/main/Main';
 import Info from '@/components/pages/home-page/info/Info';
 import CarPark from '@/components/pages/home-page/car-park/CarPark';
-import GpsPage from '@/components/pages/home-page/gps-page/GpsPage';
+import BannerSection from '@/components/pages/home-page/banner-section/BannerSection';
 
 const HomePage = () => {
     return (
@@ -10,7 +10,26 @@ const HomePage = () => {
             <Main />
             <Info />
             <CarPark />
-            <GpsPage />
+            <BannerSection
+                bg={'./backgrounds/gps-bg.jpg'}
+                title={
+                    'Все наши машины оснащены системой контроля GPS-навигацией с поддержкой ГЛОНАСС'
+                }
+                text={
+                    'Мы всегда сможем отследить местонахождение Вашего груза и связаться с водителем'
+                }
+                mr={200}
+                left={true}
+            />
+            <BannerSection
+                bg={'./backgrounds/belay-bg.jpg'}
+                title={'Ваш груз застрахован на 10 млн. рублей'}
+                text={
+                    'Мы предлагаем: полный комплекс услуг по страхованию груза; полис экспедированной ответственности.'
+                }
+                ml={200}
+                right={true}
+            />
         </>
     );
 };
