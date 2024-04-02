2399b1822b1ca2150840173a73e6789c817d9b9b
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-04-02 16:34:58 +0300

    - updated images

--- src/app/vacancies/page.tsx
+++ src/app/vacancies/page.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import VacanciesBanner from '@/components/pages/vacancies/vacancies-banner/VacanciesBanner';
 import AllVacanciesSection from '@/components/pages/vacancies/all-vacancies-section/AllVacanciesSection';
 import VacanciesForm from '@/components/pages/vacancies/vacancies-form/VacanciesForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'АИВА - Вакансии',
+    description:
+        'АИВА Грузоперевозки – ваш надежный партнер в организации перевозок ' +
+        'грузов по всей России. Мы предоставляем высококачественные услуги по ' +
+        'доставке грузов различного типа и объема, обеспечивая оперативность, ' +
+        'безопасность и надежность на всех этапах транспортировки. Наша компания' +
+        ' основана на принципах профессионализма, эффективности и индивидуального' +
+        ' подхода к каждому клиенту, стремясь удовлетворить самые высокие требования.',
+    keywords:
+        'грузоперевозки, транспортная логистика, доставка грузов, вся Россия,' +
+        ' надежность, профессионализм, оперативность, безопасность,' +
+        ' индивидуальный подход, качество услуг, страховка',
+};
 
 const VacanciesPage = () => {
     return (
